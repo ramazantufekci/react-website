9c2b78c0950d7601f37c77763481ea618f0a9e2b
ramazantufekci <ramazan.tufekci@outlook.com>
2019-08-17 09:09:41 +0300

    başlangıç

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bulutsuzluk özlemi
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=LDsZcYtyu-I"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dinle
         </a>
       </header>
     </div>

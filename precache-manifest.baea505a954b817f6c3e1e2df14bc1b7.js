ae99ef2545596e100ead178ff93b0d05cfc5bddd
ramazantufekci <ramazan.tufekci@outlook.com>
2019-08-17 09:17:04 +0300

    Updates

---
+++ precache-manifest.baea505a954b817f6c3e1e2df14bc1b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656e8a71eacf8a3361b2713265ccf83f",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "83010ae257c4e52b1308",
+    "url": "/react-website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b3f8a6d996a7b52a72c3",
+    "url": "/react-website/static/js/2.46095a47.chunk.js"
+  },
+  {
+    "revision": "83010ae257c4e52b1308",
+    "url": "/react-website/static/js/main.f10cbea2.chunk.js"
+  },
+  {
+    "revision": "de696f7ad09d15d75429",
+    "url": "/react-website/static/js/runtime~main.e0941f60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
